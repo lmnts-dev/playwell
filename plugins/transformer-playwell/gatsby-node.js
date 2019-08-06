d7215b1ffed62151c2d991fea66860f9682d5c54
Peter Laxalt <pete@laxaltandmciver.co>
2019-08-06 17:40:37 -0400

    ✨ allPlayWellManager graphql query route with new api routes provided by steve

--- plugins/transformer-playwell/gatsby-node.js
+++ plugins/transformer-playwell/gatsby-node.js
@@ -4,12 +4,21 @@
 const axios = require('axios');
 const crypto = require('crypto');
 
+// Our API Routes:
+// Contact Steve Halford at PlayWell for more information.
 const CLIENT_API_URI = 'https://course-finder.play-well.org/webservice/courses';
+const MANAGER_API_URI =
+  'https://course-finder.play-well.org/webservice/managers';
 
+// Create our GraphQL Architecture
 exports.sourceNodes = async ({ actions }) => {
+  // `createNode` action is to create GraphQL nodes that we can query.
   const { createNode } = actions;
-  const result = await axios.get(CLIENT_API_URI);
-  for (const client of result.data) {
+
+  // Client API Route to GraphQL Architecture:
+  // This is to return PlayWell Clients & Courses.
+  const client_results = await axios.get(CLIENT_API_URI);
+  for (const client of client_results.data) {
     await createNode({
       children: [],
       id: client.client_location_id.toString(),
@@ -32,4 +41,39 @@ exports.sourceNodes = async ({ actions }) => {
       },
     });
   }
+
+  // Manager API Route to GraphQL Architecture:
+  // This is to return all PlayWell Managers.
+  const manager_results = await axios.get(MANAGER_API_URI);
+
+  // Create our GraphQL index of managers.
+  // Create an index so we can have an ID per manager.
+  let manager_index = 0;
+
+  for (const manager of manager_results.data) {
+    // Create an index so we can have an ID per manager.
+    manager_index = manager_index + 1;
+
+    await createNode({
+      children: [],
+      id: manager_index.toString(),
+      manager: manager.manager,
+      email: manager.email,
+      cell_number: manager.cell_number,
+      cost_code: manager.cost_code,
+      cost_code_name: manager.cost_code_name,
+      state: manager.state,
+      state_id: manager.state_id,
+      counties: manager_results.counties,
+      parent: null,
+      internal: {
+        type: 'PlayWellManagers',
+        contentDigest: crypto
+          .createHash(`md5`)
+          .update(JSON.stringify(manager))
+          .digest(`hex`),
+      },
+    });
+  }
+
 };
