b0a13fcbab1de8572cdd3a78d8ca572da4164e92
Casey K <tehcnicolour@gmail.com>
2019-09-20 01:00:14 -0700

    react-slick is much better

--- web/src/sections/HomeHero/HomeHero.js
+++ web/src/sections/HomeHero/HomeHero.js
@@ -14,17 +14,12 @@ import { Icon } from 'components/library/Icons';
 import HeroContainer from 'components/library/Hero/HeroContainer';
 import { LocationFilter } from './LocationFilter';
 
-import {
-  CarouselProvider,
-  Slider,
-  Slide,
-  ButtonBack,
-  ButtonNext,
-} from 'pure-react-carousel';
-import 'pure-react-carousel/dist/react-carousel.es.css';
+import Slider from 'react-slick';
 
 // Styles
-import { SliderContainer, SliderArrow, Actions, HeroBtn } from './styles.scss';
+import { SliderContainer, Actions, HeroBtn } from './styles.scss';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 // Constants
 import { Theme, Root } from 'constants/Theme';
@@ -38,37 +33,28 @@ const HeroProps = {
   textAlign: 'center',
 };
 
+const settings = {
+  infinite: true,
+  speed: 500,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+};
+
 // Render Page
 const HomeHero = ({ bgColor, textColor, slides }) => {
   return (
     <HeroContainer {...HeroProps}>
-      <SliderContainer width={[1, 1, 1, 1, 7 / 10]} pl={[0, 0, 2, 6, 9]}>
-        <CarouselProvider
-          naturalSlideWidth={16}
-          naturalSlideHeight={3}
-          totalSlides={3}
-        >
-          <Slider>
-            {slides.map(slide => (
-              <Slide index={slide.index} key={slide.message}>
-                <Text as="h2">{slide.message}</Text>
-                <Text as="span" fontSize={3} color="Primary" pb={2}>
-                  {slide.detail}
-                </Text>
-              </Slide>
-            ))}
-          </Slider>
-          <ButtonBack>
-            <SliderArrow>
-              <Icon Name="carat" />
-            </SliderArrow>
-          </ButtonBack>
-          <ButtonNext>
-            <SliderArrow className="next-slide">
-              <Icon Name="carat" />
-            </SliderArrow>
-          </ButtonNext>
-        </CarouselProvider>
+      <SliderContainer width={[1, 1, 1, 1, 6 / 10]} pl={[0, 0, 2, 6, 9]}>
+        <Slider {...settings}>
+          {slides.map(slide => (
+            <Box index={slide.index} key={slide.message}>
+              <Text as="h2">{slide.message}</Text>
+              <Text as="span" fontSize={3} color="Primary" pb={2}>
+                {slide.detail}
+              </Text>
+            </Box>
+          ))}
+        </Slider>
         <Actions width={[1, 1, 1, 7 / 10]} pl={[0, 0, 2, 6, 9]}>
           <HeroBtn bgColor={Theme.Color.Nova} textColor={Theme.Color.White}>
             Explore Courses
