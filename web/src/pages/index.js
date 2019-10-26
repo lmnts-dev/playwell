33b13ec06537799d5d621f5fafb1ac0ea0ee028d
Casey K <tehcnicolour@gmail.com>
2019-10-25 21:18:08 -0700

    fixes image clip

--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -43,7 +43,7 @@ const ThemeProps = {
 
 const Centered = styled(CenteredTitle)`
   &.community {
-    ~.gatsby-image-wrapper {
+    ~ .gatsby-image-wrapper {
       height: 130%;
       width: 100%;
       position: absolute !important;
@@ -89,15 +89,16 @@ const HomePage = () => (
             Class="h3"
             className="community"
           />
-          <ImgMatch src="cloud-big.png" objectFit="contain" objectPosition="left center"/>
+          <ImgMatch
+            src="cloud-big.png"
+            objectFit="contain"
+            objectPosition="left center"
+          />
         </BasicInner>
         <HomeCommunity slides={CommunitySlides} />
       </BasicSection>
 
-      <BasicSection
-        BgColor={Theme.Color.Ocean}
-        TextColor={Theme.Color.White}
-      >
+      <BasicSection BgColor={Theme.Color.Ocean} TextColor={Theme.Color.White}>
         <BasicInner noPaddingLeft>
           <EdgeImageWithText
             Reversed
@@ -154,40 +155,53 @@ const HomePage = () => (
       >
         <ImageAccordianFeature
           Links={[
-            { link: '/', label: 'Robotics' }, 
-            { link: '/', label: 'Intro to STEM' }, 
-            { link: '/', label: 'Gaming' }, 
-            { link: '/', label: 'Advanced Engineering' }, 
-            { link: '/', label: 'Engineering Themes' }, 
+            { link: '/', label: 'Robotics' },
+            { link: '/', label: 'Intro to STEM' },
+            { link: '/', label: 'Gaming' },
+            { link: '/', label: 'Advanced Engineering' },
+            { link: '/', label: 'Engineering Themes' },
           ]}
           Accordians={[
-            { header: 'Worshops', paragraph: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', image:'girl-3.jpg' }, 
-            { header: 'Classes', paragraph: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', image:'girl-2.jpg' },
-            { header: 'Camps', paragraph: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', image:'girl-1.jpg' },
+            {
+              header: 'Worshops',
+              paragraph:
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+              image: 'girl-3.jpg',
+            },
+            {
+              header: 'Classes',
+              paragraph:
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+              image: 'girl-2.jpg',
+            },
+            {
+              header: 'Camps',
+              paragraph:
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+              image: 'girl-1.jpg',
+            },
           ]}
         />
       </BasicSection>
-
-      <BasicSection
-        BgColor={Theme.Color.Background}
-        TextColor={Theme.Color.Dino}
-        noPaddingTop
-        noPaddingBottom
-      >
-        <BasicInner>
-          <SpecialSimpleTextWithImage
-            Caption="It starts with play"
-            Header="Build Your Own World of Opportunity"
-            Paragraph="Bring your kid over or bring out your inner kid with some of our online activities to feed your imagination."
-            Image="lego-ladder.png"
-            ButtonText="Play Zone"
-            ButtonDest="/"
-            className="overflow-image"
-          />
-        </BasicInner>
-      </BasicSection>
-
     </Main>
+    <BasicSection
+      BgColor={Theme.Color.Background}
+      TextColor={Theme.Color.Dino}
+      noPaddingTop
+      noPaddingBottom
+    >
+      <BasicInner>
+        <SpecialSimpleTextWithImage
+          Caption="It starts with play"
+          Header="Build Your Own World of Opportunity"
+          Paragraph="Bring your kid over or bring out your inner kid with some of our online activities to feed your imagination."
+          Image="lego-ladder.png"
+          ButtonText="Play Zone"
+          ButtonDest="/"
+          className="overflow-image"
+        />
+      </BasicInner>
+    </BasicSection>
   </Layout>
 );
 
@@ -230,11 +244,11 @@ const SpecialSimpleTextWithImage = styled(SimpleTextWithImage)`
       }
     }
   }
-`
+`;
 
 // Styles
 const Main = styled.main`
-  overflow: hidden;
+  overflow-x: hidden;
 `;
 
 const Decorator = styled(Box)`
