ca49c63cf60d252c657693629c5858bb896730ec
Peter Laxalt <pete@laxaltandmciver.co>
2019-11-27 19:28:49 -0500

    successfully migrate mapboxGl api to a hire order component to dynamically filter listings based on map data + pass previous map coordinates to <Link /> via reach router for smooth navigation between locations

--- web/src/components/library/CourseListings/CourseListings.js
+++ web/src/components/library/CourseListings/CourseListings.js
@@ -7,6 +7,9 @@
 // Core
 import React, { PureComponent } from 'react';
 import { Link } from 'gatsby';
+import mapboxgl from 'mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+import { navigate } from '@reach/router';
 
 // Constants
 import { Base } from 'constants/styles/Base';
@@ -18,28 +21,333 @@ import { ListingsCounters } from './ListingsCounters';
 import { ListingsFilters } from './ListingsFilters';
 import { ClientCard } from './ClientCard';
 import { CourseHero } from './CourseHero';
-import { CourseMapNav } from 'components/library/CourseMapNav';
+import { CourseMapNavContainer } from 'components/library/CourseMapNav';
 
 // Helpers
 import slugify from 'helpers/slugify';
 import locationMatch from 'helpers/locationMatch';
 import withLocation from 'helpers/withLocation';
 import {
-  getCenterFromDegrees,
+  clientGeoJsonAdapter,
   clientsByLatLong,
-} from 'helpers/getCenterFromDegrees';
+} from 'helpers/clientGeoJsonAdapter';
 import { costCodesAdapter } from 'helpers/costCodesAdapter';
 
 // Data
 import { DataFetch } from 'hooks/DataFetch';
 
 // Styles
-import { CourseListingsStyle, ListingsResultsStyle } from './styles.scss';
+import {
+  CourseListingsStyle,
+  ListingsResultsStyle,
+  CourseMapNavStyle,
+  ToggleMapBtnStyle,
+} from './styles.scss';
 
 // Begin Component
 //////////////////////////////////////////////////////////////////////
 
-// Our Filtered Results component
+/**
+ *
+ * CourseMapNav
+ *
+ */
+
+mapboxgl.accessToken =
+  'pk.eyJ1IjoicGV0ZXJsYXhhbHQiLCJhIjoiY2p6cTExcmw5MHZpNTNubW1wc25veXJseiJ9.3nFjkPOZM2kbHYQCdkqU7g';
+
+const ToggleMapBtn = () => {
+  return (
+    <ToggleMapBtnStyle>
+      <Icon Name="expand" fas />
+      <span>Expand Map</span>
+    </ToggleMapBtnStyle>
+  );
+};
+
+class CourseMapNav extends PureComponent {
+  constructor(props) {
+    super(props);
+
+    // Initial State
+    this.state = {
+      lat: 40.7088,
+      lng: -73.9888,
+      zoom: 2,
+    };
+
+    // Create our Map Ref
+    this.mapBoxRef = React.createRef();
+  }
+
+  /**
+   *
+   * Initialize Our Map
+   *
+   */
+
+  componentDidMount() {
+    const clientEdges = this.props.courseData.allPlayWellClient.edges;
+    const stateId = this.props.pageContext.isCostCode // If CostCode:
+      ? this.props.pageContext.parentState.playwell_state_id // Display State Id.
+      : this.props.pageContext.isCounty // If County:
+      ? this.props.pageContext.parentState.playwell_state_id // Display State Id.
+      : this.props.pageContext.playwell_state_id; // else it's a State and remove parentState and use it's Id.
+
+    const countyId = this.props.pageContext.isCostCode // If CostCode:
+      ? this.props.pageContext.county_id // Display County Id.
+      : this.props.pageContext.isCounty // If County:
+      ? this.props.pageContext.county_id // Display County Id.
+      : null; // else it's a State and County Id is no longer needed.
+
+    const costCodeId = this.props.pageContext.isCostCode // If CostCode:
+      ? this.props.pageContext.cost_code // Display Cost Code.
+      : this.props.pageContext.isCounty // If County:
+      ? this.props.pageContext.cost_code // Display Cost Code.
+      : null; // else it's a State and CostCode is no longer needed.
+
+    /**
+     *
+     * For Debugging Only
+     *
+     */
+    // console.log('clientEdges:', clientEdges);
+    // console.log(
+    //   'clientsByLatLong(7, clientEdges):',
+    //   clientsByLatLong(7, clientEdges)
+    // );
+    // console.log(
+    //   'getCenterFromDegrees(clientsByLatLong(7, clientEdges)):',
+    //   getCenterFromDegrees(clientsByLatLong(7, clientEdges))
+    // );
+
+    // Initial position
+    const { lng, lat, zoom } = this.state;
+
+    // Our map object
+    const map = new mapboxgl.Map({
+      container: this.mapBoxRef,
+      style: 'mapbox://styles/mapbox/streets-v9',
+      maxZoom: 11,
+    });
+
+    // Updating state depending on map position & zoom
+    map.on('move', () => {
+      const { lng, lat } = map.getCenter();
+
+      this.setState({
+        lng: lng.toFixed(4),
+        lat: lat.toFixed(4),
+        zoom: map.getZoom().toFixed(2),
+      });
+    });
+
+    /**
+     *
+     * Generate our map markers
+     *
+     */
+
+    // Our data
+    const clientsGeoJson = clientGeoJsonAdapter(
+      this.props.courseData.allPlayWellClient.edges
+    );
+
+    // Map Id
+    const mapId = 'clients';
+
+    /**
+     *
+     * Once  our map is loaded:
+     *
+     */
+    map.on('load', () => {
+      // Add zoom and rotation controls to the map.
+      map.addControl(new mapboxgl.NavigationControl());
+
+      /**
+       *
+       * Fit to our respective initial bounds provided by pageContext
+       *
+       */
+
+      // First establish an empty set of bounds.
+      const initialBounds = new mapboxgl.LngLatBounds();
+
+      // Create raw markers with our helper function and utilize our State Id from CourseListings / PageContext.
+      const initialMarkers = clientsByLatLong(
+        stateId,
+        countyId,
+        costCodeId,
+        clientEdges
+      );
+
+      // Extend our bounds to include our raw markers
+      initialMarkers.forEach(coordinates => {
+        initialBounds.extend(coordinates);
+      });
+
+      // Fit our map to said bounds.
+      map.fitBounds(initialBounds, { padding: 50 });
+
+      /**
+       *
+       * Add data layers.
+       *
+       */
+      map.addLayer({
+        id: mapId,
+        type: 'circle',
+        source: {
+          type: 'geojson',
+          data: {
+            type: 'FeatureCollection',
+            features: clientsGeoJson,
+          },
+        },
+        paint: {
+          'circle-radius': 5,
+          'circle-color': Theme.Color.Nova,
+          'circle-stroke-color': Theme.Color.White,
+          'circle-stroke-width': 1,
+          'circle-opacity': 1,
+        },
+      });
+
+      // When a click event occurs on a feature in the places layer, open a popup at the
+      // location of the feature, with description HTML from its properties.
+      map.on('click', mapId, function(e) {
+        var coordinates = e.features[0].geometry.coordinates.slice();
+        var description = e.features[0].properties.title;
+
+        // Ensure that if the map is zoomed out such that multiple
+        // copies of the feature are visible, the popup appears
+        // over the copy being pointed to.
+        while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+          coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+        }
+
+        new mapboxgl.Popup()
+          .setLngLat(coordinates)
+          .setHTML(description)
+          .addTo(map);
+      });
+
+      // Change the cursor to a pointer when the mouse is over the places layer.
+      map.on('mouseenter', mapId, function() {
+        map.getCanvas().style.cursor = 'pointer';
+      });
+
+      // Change it back to a pointer when it leaves.
+      map.on('mouseleave', mapId, function() {
+        map.getCanvas().style.cursor = '';
+      });
+
+      // // Remove popup after hover
+      // map.on('mouseleave', mapId, () => {
+      //   map.getCanvas().style.cursor = '';
+      //   popup.remove();
+      // });
+
+      // Functions to run on click.
+      map.on('click', mapId, function(e) {
+        // Fly to point
+        map.flyTo({ center: e.features[0].geometry.coordinates, zoom: 9 });
+
+        // Use Reach Router to update route.
+        navigate(`?location=${e.features[0].properties.locationHash}`);
+      });
+    });
+  }
+
+  /**
+   *
+   * Unmounted state
+   *
+   */
+
+  componentWillUnmount() {
+    this.state = {
+      lat: 40.7088,
+      lng: -73.9888,
+      zoom: 10.6,
+    };
+  }
+
+  render() {
+    const mapWidth = this.props.mapWidth;
+    const mapZedIndex = this.props.mapZedIndex;
+    const { lng, lat, zoom } = this.state;
+
+    /**
+     *
+     * For Debugging Only
+     *
+     */
+    // console.log(
+    //   'clientGeoJsonAdapter(this.props.courseData.allPlayWellClient.edges)',
+    //   clientGeoJsonAdapter(this.props.courseData.allPlayWellClient.edges)
+    // );
+    // console.log('lat: ' + lat);
+    // console.log('lng: ' + lng);
+    // console.log('zoom: ' + zoom);
+
+    return (
+      <CourseMapNavContainer
+        mapZedIndex={mapZedIndex}
+        mapWidth={mapWidth}
+        mapBoxRef={el => (this.mapBoxRef = el)}
+      />
+    );
+  }
+}
+
+// this.mapBoxRef
+
+// const CourseMapNavContainer = ({
+//   lat,
+//   lng,
+//   zoom,
+//   courseData,
+//   mapWidth,
+//   mapZedIndex,
+//   stateId,
+//   countyId,
+//   costCodeId,
+//   pageContext,
+//   mapBoxRef,
+// }) => {
+//   return (
+//     <CourseMapNavStyle mapZedIndex={mapZedIndex} mapWidth={mapWidth}>
+//       <div className="inner">
+//         <div className="map-col">
+//           <div className="map-container">
+//             <ToggleMapBtn />
+//             <div className="map-container-inner">
+//               <div
+//                 ref={mapBoxRef}
+//                 style={{
+//                   position: 'absolute',
+//                   left: '0',
+//                   right: '0',
+//                   bottom: '0',
+//                   top: '0',
+//                 }}
+//               />
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </CourseMapNavStyle>
+//   );
+// };
+
+/**
+ *
+ * Our Filtered Results component
+ *
+ */
+
 class FilteredResults extends PureComponent {
   constructor(props) {
     super(props);
@@ -296,7 +604,11 @@ class FilteredResults extends PureComponent {
   }
 }
 
-// Our Listing Results
+/**
+ *
+ * Our Listing Results
+ *
+ */
 class ListingsResults extends PureComponent {
   constructor(props) {
     super(props);
@@ -424,8 +736,7 @@ class ListingsResults extends PureComponent {
  *
  *  The wrapper around our listings
  *
- *  */
-
+ */
 const ListingsWrapper = ({ courseData, mapWidth, mapZedIndex, children }) => {
   return (
     <CourseListingsStyle mapZedIndex={mapZedIndex} mapWidth={mapWidth}>
@@ -440,8 +751,7 @@ const ListingsWrapper = ({ courseData, mapWidth, mapZedIndex, children }) => {
  *
  * The Page Itself
  *
- **/
-
+ */
 class CourseListings extends PureComponent {
   constructor(props) {
     super(props);
@@ -548,7 +858,10 @@ class CourseListings extends PureComponent {
         currentlyViewing: safeUrlLocationQuery,
       });
 
-      console.log('TRUE this.state.currentlyViewing:', this.state.currentlyViewing);
+      console.log(
+        'TRUE this.state.currentlyViewing:',
+        this.state.currentlyViewing
+      );
     } else {
       console.log('FALSE this.state.currentlyViewing:', 'no query');
     }
--- web/src/components/library/CourseListings/styles.scss.js
+++ web/src/components/library/CourseListings/styles.scss.js
@@ -116,4 +116,120 @@ export const ListingsResultsStyle = styled.div`
   }
 `;
 
+/**
+ *
+ * Course Map Nav
+ *
+ */
+
+export const CourseMapNavStyle = styled.div`
+  position: fixed;
+  left: 0;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  animation: ${FadeIn} 1s ease 0s 1 normal forwards;
+  z-index: ${props => (props.mapZedIndex ? props.mapZedIndex : 4)};
+  display: flex;
+  flex-direction: column;
+
+  @media (max-width: ${Theme.Base.Media.Width.Md}) {
+    display: none;
+  }
+
+  .mapboxgl-popup {
+    max-width: 200px;
+  }
+
+  .inner {
+    max-width: ${Root.Site.Width};
+    width: 100%;
+    margin: 0 auto;
+    padding-left: ${Root.Grid.Gutter.Left};
+    padding-right: ${Root.Grid.Gutter.Right};
+    position: relative;
+    display: flex;
+    flex: 1;
+    justify-content: flex-end;
+
+    .map-col {
+      display: flex;
+      flex-direction: column;
+      padding-top: calc(${props => Root.Nav.Size} + ${props => Root.Size} * 1);
+      width: ${props => (props.mapWidth ? props.mapWidth : '350px')};
+
+      .map-container {
+        position: relative;
+        display: flex;
+        align-items: stretch;
+        justify-content: stretch;
+        box-shadow: 0 12px 44px 0 ${hexToRGB(Theme.Color.Galaxy, 0.11)};
+        background: ${Theme.Color.Sky};
+        overflow: hidden;
+        color: white;
+        font-weight: bold;
+        border-radius: 5px;
+        height: calc(
+          100% - (${props => Root.Nav.Size} + ${props => Root.Size} * 1)
+        );
+        width: 100%;
+      }
+
+      .map-container-inner {
+        display: flex;
+        flex: 1;
+        position: relative;
+
+        .map {
+          position: absolute;
+          left: 0;
+          right: 0;
+          bottom: 0;
+          top: 0;
+        }
+      }
+    }
+  }
+`;
+
+export const ToggleMapBtnStyle = styled.button`
+  background: ${Theme.Color.White};
+  border-radius: 4px;
+  color: ${Theme.Color.Galaxy};
+  position: absolute;
+  left: calc(${Root.Size} / 6);
+  top: calc(${Root.Size} / 6);
+  font-weight: bold;
+  height: ${Root.Button.Size};
+  display: inline-flex;
+  align-items: center;
+  padding: 0 calc(${Root.Size} / 4) 0 calc(${Root.Size} / 6);
+  display: inline-flex;
+  justify-content: space-between;
+  align-items: center;
+  cursor: pointer;
+  outline: 0;
+  border: 0;
+  z-index: 800;
+  box-shadow: 0px 0px 0px 0px rgba(${Theme.Color.Nightysky}, 0);
+  transition: all 0s ease;
+
+  .ico {
+    color: ${Theme.Color.Galaxy};
+    display: block;
+    margin-right: calc(${Root.Size} / 4);
+  }
+
+  span {
+    &:last-child {
+      position: relative;
+      top: 2px;
+    }
+  }
+
+  &:hover {
+    box-shadow: 0px 0px 0px 6px rgba(93, 99, 118, 0.2);
+  }
+`;
+
 //////////////////////////////////////////////////////////////////////
