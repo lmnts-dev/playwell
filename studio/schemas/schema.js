5e5421567217c18dc6ec1ffa24c57b19f3837e00
Jason Cory Alvernaz <jason.alvernaz@mutations.ltd>
2020-01-18 20:09:35 -0800

    Categorize document and object schemas

--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -5,10 +5,10 @@ import createSchema from 'part:@sanity/base/schema-creator'
 import schemaTypes from 'all:part:@sanity/base/schema-type'
 
 // We import object and document schemas
-import blockContent from './blockContent'
-import category from './category'
-import post from './post'
-import author from './author'
+import blockContent from './objects/blockContent'
+import category from './documents/category'
+import post from './documents/post'
+import author from './documents/author'
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
