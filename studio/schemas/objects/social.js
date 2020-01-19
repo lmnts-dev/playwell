8ba62ef05432a84cb37d1d8a8459e556a1092fb1
Jason Cory Alvernaz <jason.alvernaz@mutations.ltd>
2020-01-19 13:30:04 -0800

    Add siteMetadata to schema

---
+++ studio/schemas/objects/social.js
@@ -0,0 +1,27 @@
+export default {
+    title: 'Social',
+    name: 'social',
+    type: 'object',
+    fields: [
+        {
+            title: 'Twitter',
+            name: 'twitter',
+            type: 'string'
+        },
+        {
+            title: 'Facebook',
+            name: 'facebook',
+            type: 'string'
+        },
+        {
+            title: 'Instagram',
+            name: 'instagram',
+            type: 'string'
+        },
+        {
+            title: 'Facebook App ID',
+            name: 'fbAppId',
+            type: 'string'
+        }
+    ]
+}
