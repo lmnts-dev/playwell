f0d012031afa6df5f3a8ddd5eaf9edb74d3b7856
Jason Cory Alvernaz <jason.alvernaz@mutations.ltd>
2020-01-19 12:39:20 -0800

    Create nav schema to nest focus, primary, footer

---
+++ studio/schemas/objects/nav.js
@@ -0,0 +1,22 @@
+export default {
+    title: 'Nav',
+    name: 'nav',
+    type: 'object',
+    fields: [
+        {
+            title: 'Focus Nav',
+            name: 'focusNav',
+            type: 'focusNav'
+        },
+        {
+            title: 'Primary Nav',
+            name: 'primaryNav',
+            type: 'primaryNav'
+        },
+        {
+            title: 'Footer Nav',
+            name: 'footerNav',
+            type: 'footerNav'
+        }
+    ]
+}
