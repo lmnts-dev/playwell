79c1dd3a039d0216b5fcaf0a9e758a30bcaa389f
Jason Cory Alvernaz <jason.alvernaz@mutations.ltd>
2020-01-19 12:45:06 -0800

    Create siteSettings document

---
+++ studio/schemas/documents/siteSettings.js
@@ -0,0 +1,12 @@
+export default {
+    title: 'Site Settings',
+    name: 'siteSettings',
+    type: 'document',
+    fields: [
+        {
+            title: 'Site Navigation',
+            name: 'nav',
+            type: 'nav'
+        }
+    ]
+}
