a7ae731800cafbf270fe942306e377267580da4f
Jason Cory Alvernaz <jason.alvernaz@mutations.ltd>
2020-02-09 14:17:40 -0800

    Make nested schema for state, county, and costCode

---
+++ studio/schemas/documents/state.js
@@ -0,0 +1,33 @@
+export default {
+  name: 'state',
+  title: 'State',
+  type: 'document',
+  fields: [
+    {
+      title: 'Name',
+      name: 'name',
+      type: 'string'
+    },
+    {
+      title: 'Description',
+      name: 'description',
+      type: 'string'
+    },
+    {
+      title: 'Subheadline',
+      name: 'subheadline',
+      type: 'string'
+    },
+    {
+      title: 'Cover Image',
+      name: 'coverImage',
+      type: 'image'
+    },
+    {
+      title: 'Counties',
+      name: 'counties',
+      type: 'array',
+      of: [{type: 'county'}]
+    }
+  ]
+}
--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -7,12 +7,14 @@ import schemaTypes from 'all:part:@sanity/base/schema-type'
 // Import document schemas
 import author from './documents/author'
 import category from './documents/category'
-import location from './documents/location'
 import post from './documents/post'
 import siteSettings from './documents/siteSettings'
+import state from './documents/state'
 
 // Import the object schemas
 import blockContent from './objects/blockContent'
+import costCode from './objects/costCode'
+import county from './objects/county'
 import focusLinkItem from './objects/navigation/focusLinkListItem'
 import focusNav from './objects/navigation/focusNav'
 import footerNav from './objects/navigation/footerNav'
@@ -39,10 +41,12 @@ export default createSchema({
     category,
     post,
     siteSettings,
-    location,
+    state,
     // When added to this list, object types can be used as
     // { type: 'typename' } in other document schemas
     blockContent,
+    costCode,
+    county,
     focusLinkItem,
     focusNav,
     footerNav,
