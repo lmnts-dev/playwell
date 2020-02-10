f33def758c55a71aaac72bba4bd275d7a8c3c168
Jason Cory Alvernaz <jason.alvernaz@mutations.ltd>
2020-02-09 17:54:10 -0800

    Sort documents and objects alphabetically

--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -4,7 +4,13 @@ import createSchema from 'part:@sanity/base/schema-creator'
 // Then import schema types from any plugins that might expose them
 import schemaTypes from 'all:part:@sanity/base/schema-type'
 
-// We import object and document schemas
+// Import document schemas
+import author from './documents/author'
+import category from './documents/category'
+import post from './documents/post'
+import siteSettings from './documents/siteSettings'
+
+// Import the object schemas
 import blockContent from './objects/blockContent'
 import focusLinkItem from './objects/navigation/focusLinkListItem'
 import focusNav from './objects/navigation/focusNav'
@@ -18,10 +24,6 @@ import siteMetadata from './objects/siteMetadata'
 import social from './objects/social'
 import subNav from './objects/navigation/subNav'
 import theme from './objects/navigation/theme'
-import category from './documents/category'
-import post from './documents/post'
-import author from './documents/author'
-import siteSettings from './documents/siteSettings'
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -32,9 +34,9 @@ export default createSchema({
   types: schemaTypes.concat([
     // The following are document types which will appear
     // in the studio.
-    post,
     author,
     category,
+    post,
     siteSettings,
     // When added to this list, object types can be used as
     // { type: 'typename' } in other document schemas
