b6872184e3995b30fdf35f15278aeb81bf2a4197
Jason Cory Alvernaz <jason.alvernaz@mutations.ltd>
2020-02-14 15:13:25 -0800

    Transform data to match Sanity schema

--- web/scripts/sanityClient.js
+++ web/scripts/sanityClient.js
@@ -16,4 +16,41 @@ const getSiteSettings = async () => {
   return siteSettings
 }
 
+const transformPlaywellStates = (state, counties) => {
+  const countiesRef = counties.map(county => {
+    return {
+      _type: 'reference',
+      _ref: `playwellCounty-${county.county_id.toString()}`,
+      _key: `playwellCounty-${county.county_id.toString()}`
+    }
+  })
+  const sanityState = {
+    _id: `playwellState-${state.state_id.toString()}`,
+    stateId: state.state_id.toString(),
+    _type: `state`,
+    name: state.name,
+    counties: countiesRef
+  }
+  return sanityState
+}
+
+const transformPlaywellCounties = (county) => {
+  const sanityCounty = {
+    _id: `playwellCounty-${county.county_id.toString()}`,
+    countyId: county.county_id.toString(),
+    _type: `county`,
+    name: county.name,
+    costCode: {_type: 'reference', _ref: `playwellCostCode-${county.cost_code.toString()}`}
+  }
+  const sanityCostCode = {
+    _id: `playwellCostCode-${county.cost_code.toString()}`,
+    costCodeId: county.cost_code.toString(),
+    _type: `costCode`,
+    name: county.cost_code_name,
+  }
+  return [sanityCostCode, sanityCounty]
+}
+
 module.exports.getSiteSettings = getSiteSettings
+module.exports.transformPlaywellStates = transformPlaywellStates
+module.exports.transformPlaywellCounties = transformPlaywellCounties
