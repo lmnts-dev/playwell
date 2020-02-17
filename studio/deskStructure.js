24269612477e5d82b52564215dde579cda6e4187
Jason Cory Alvernaz <jason.alvernaz@mutations.ltd>
2020-02-17 15:42:42 -0800

    Add deskStructure and singleton for siteSettings

---
+++ studio/deskStructure.js
@@ -0,0 +1,18 @@
+import S from '@sanity/desk-tool/structure-builder'
+
+export default () =>
+  S.list()
+    .title('Content')
+    .items([
+      S.listItem()
+        .title('Settings')
+        .icon(MdSettings)
+        .child(
+          S.editor()
+            .id('siteSettings')
+            .schemaType('siteSettings')
+            .documentId('siteSettings')
+        ),
+      ...S.documentTypeListItems().filter(listItem => !['siteSettings', 'state', 'county', 'costCode'].includes(listItem.getId()))
+    ]
+    )
